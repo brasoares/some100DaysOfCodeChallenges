7e0fca5ad70565a8b29f85e87cc2114501636b58
Henoc Soares Freire <henocsoares@hotmail.com.br>
2023-12-01 02:20:06 -0300

    Create average_grade.py
    int function can also be applied at the beginning. Easier?

---
+++ Python™Brasil/estrutura_sequencial/average_grade.py
@@ -0,0 +1,10 @@
+# Create a program that asks for the 4 quarterly grades and displays the average.
+# Let's gooooooooooo!
+grade1 = input("Enter the first grade: ")
+grade2 = input("Enter the second grade: ")
+grade3 = input("Enter the third grade: ")
+grade4 = input("Enter the fourth grade: ")
+
+avg = (int(grade1) + int(grade2) + int(grade3) + int(grade4)) / 4
+
+print(f"The avarage for the quarterly grades is: {avg}")
