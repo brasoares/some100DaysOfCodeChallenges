a37bf5dc433ba855d4db5259307d34dbb1dc3ca7
Henoc Soares Freire <henocsoares@hotmail.com.br>
2023-12-01 02:14:05 -0300

    Create a_plus_b.py
    Sum of two entered #s.

---
+++ Python™Brasil/estrutura_sequencial/a_plus_b.py
@@ -0,0 +1,8 @@
+# Make a program that asks for two numbers and prints the sum.
+
+a = input("Insert the first number: ")
+b = input("Insert the second number: ")
+
+sum = int(a) + int(b)
+
+print(f"The sum of {a} + {b} = {sum}")
