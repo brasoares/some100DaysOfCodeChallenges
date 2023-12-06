b48de92da94df57ba483510e3aa0121c352f9ce1
Henoc Soares Freire <henocsoares@hotmail.com.br>
2023-12-06 19:10:11 -0300

    Create higher_than_all.py
    Whole.

---
+++ Python™Brasil/estrutura_de_decisao/higher_than_all.py
@@ -0,0 +1,14 @@
+# Create a program that reads three numbers and displays the largest among them.
+
+n1 = float(input("Enter the first number: "))
+n2 = float(input("Enter the second number: "))
+n3 = float(input("Enter the third number: "))
+
+if n1 > n2 and n1 > n3:
+	print(f"{n1:.0f}")
+elif n2 > n1 and n2 > n3:
+	print(f"{n2:.0f}")
+elif n1 == n2 and n1 == n3:
+	print(f"Identical numbers entered thrice: {n2:.0f}")
+else:
+	print(f"{n3:.0f}")
