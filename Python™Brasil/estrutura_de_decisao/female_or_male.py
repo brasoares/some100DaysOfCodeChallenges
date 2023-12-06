cc943ce12aaf79dcecbee508b38b2ba4ccb376df
Henoc Soares Freire <henocsoares@hotmail.com.br>
2023-12-06 02:21:00 -0300

    Create female_or_male.py
    Ready code.

---
+++ Python™Brasil/estrutura_de_decisao/female_or_male.py
@@ -0,0 +1,10 @@
+#Create a program that checks if a typed letter is 'F' or 'M'. Depending on the letter, write: F - Female, M - Male, Invalid Gender.
+
+gender = input("Enter F for female or M for male: ")
+
+if gender == "F":
+	print("F - Female")
+elif gender == "F":
+	print("M - Male")
+else:
+	print("Invalid Gender")
