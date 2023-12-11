ba03d97a29eaf6f888f029dc9d864fdfabbddd15
Henoc Soares Freire <henocsoares@hotmail.com.br>
2023-12-11 00:06:36 -0300

    Update quadratic_solver.py
    Improvements!

--- Python™Brasil/estrutura_de_decisao/quadratic_solver.py
+++ Python™Brasil/estrutura_de_decisao/quadratic_solver.py
@@ -14,12 +14,11 @@ a = float(input("Enter a's value: "))
 b = float(input("Enter b's value: "))
 c = float(input("Enter c's value: "))
 
+delta = pow(b, 2) - 4*a*c 
+
 if a == 0:
 	print("End of execution; not a quadratic equation (check a's value)!")
-else:
-	delta = pow(b, 2) - 4*a*c 
-
-if delta == 0:
+elif delta == 0:
 	if a != 0:	
 		root = -b / (2*a)
 		print(f"The equation has a single real root. Delta = 0 and the root is {root}.")
