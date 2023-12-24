b529f319c102bbad4a9795ae0008ce9c17e3377f
Henoc Soares Freire <henocsoares@hotmail.com.br>
2023-12-23 22:47:16 -0300

    Create lists_creation.py
    Reviewing and diving further into lists in Python.

---
+++ miscellaneous/lists_creation.py
@@ -0,0 +1,17 @@
+"""Python program to demonstrate the creation of Lists"""
+# As of https://www.geeksforgeeks.org/python-lists/
+# Basic List
+List = []
+print("Blank List: ")
+print(List)
+
+# List of numbers
+List = [10, 20, 14]
+print("\nList of numbers: ")
+print(List)
+
+# List of strings and access via 'index'
+List = ["Geeks", "for", "Geeks"]
+print("\List Items: ")
+print(List[0])
+print(List[2])
