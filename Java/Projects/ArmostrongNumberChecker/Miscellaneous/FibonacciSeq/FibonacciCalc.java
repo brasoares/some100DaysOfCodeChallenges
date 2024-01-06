531249828d560c82706629b4d97e54d12b6220a6
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-06 00:10:52 -0300

    Update FibonacciCalc.java
    Modifications... it seems there is more to be done!

--- Java/Projects/ArmostrongNumberChecker/Miscellaneous/FibonacciSeq/FibonacciCalc.java
+++ Java/Projects/ArmostrongNumberChecker/Miscellaneous/FibonacciSeq/FibonacciCalc.java
@@ -1,11 +1,26 @@
 public class FibonacciCalc {
-    // Method to create and display the seq
+    // Method to generate and display the sequence
     public void genAndDisplaySeq(int n) {
-        for (int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             int fiboNum = calcFibo(i);
             System.out.printf("Fibonacci number at index %d: %d%n", i, fiboNum);
         }
+    }
 
-        System.out.println("The sequence of requested %d numbers: %d", n, genAndDisplaySeq)
+    // This method actually calculates the Fibonacci sequence
+    private int calcFibo(int index) {
+        if (index <= 1) {
+            return index;
+        } else {
+            int a = 0;
+            int b = 1;
+            int result = 0;
+            for (int i = 2; i <= index; i++) {
+                result = a + b;
+                a = b;
+                b = result;
+            }
+            return result;
+        }
     }
 }
