4854d5c1f6f4fd714153538040ac476119f6e433
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-06 00:11:59 -0300

    Update fibonacciSeq.java
    I'll check later on to make it work! Tks a million!

--- Java/Projects/ArmostrongNumberChecker/Miscellaneous/FibonacciSeq/fibonacciSeq.java
+++ Java/Projects/ArmostrongNumberChecker/Miscellaneous/FibonacciSeq/fibonacciSeq.java
@@ -3,14 +3,20 @@ import java.util.Scanner;
 public class FibonacciSeq {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
+
         System.out.print("Enter the number of Fibonacci terms: ");
         int n = scan.nextInt();
-        
+
         // Closing the scanner to avoid resource leak
         scan.close();
-        
+
         // Call a method to perform the needed actions
         genAndDisplaySeq(n);
+    }
+
+    // Method to generate and display the Fibonacci sequence
+    private static void genAndDisplaySeq(int n) {
+        FibonacciCalc fibonacciCalc = new FibonacciCalc();
+        fibonacciCalc.genAndDisplaySeq(n);
     }
 }
