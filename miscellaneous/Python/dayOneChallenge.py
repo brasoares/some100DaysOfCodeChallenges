ac2299a7d8e20da244ee478517b1e3e365b5a27a
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-08 15:15:47 -0300

    Update dayOneChallenge.py
    Simplifying the view!

--- miscellaneous/Python/dayOneChallenge.py
+++ miscellaneous/Python/dayOneChallenge.py
@@ -1,5 +1,9 @@
-#write the given text (or similar, in my case) using, preferably, one only print statement
-
-print(
-    "Henoc\nAugust 9th 2058\nI am signing up for Replit's 100 days of Python challenge!\nI will make sure to spend some time every day coding along, for a minimum of 10 minutes a day. I'll be (also) using Replit, an amazing online IDE so I can do this (also) from my phone (if necessary) wherever I happen to be.\nNo excuses for not coding from the ISS!\nI am feeling 🥳\nYou can follow my progress at replit.com/@soaresbra"
-)
+print("""
+Henoc
+August 9th 2058
+I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day. I'll be (also) using Replit, an amazing online IDE so I can do this (also) from my phone (if necessary) wherever I happen to be.
+No excuses for not coding from the ISS!
+I am feeling 🥳
+You can follow my progress at replit.com/@soaresbra
+""")
