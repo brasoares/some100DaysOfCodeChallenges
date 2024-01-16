47db3094b021b384206c6dac576708c92ca9368f
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-15 21:55:24 -0300

    Create ex006.py

---
+++ miscellaneous/Python/basic_examples_for_review/ex006.py
@@ -0,0 +1,7 @@
+'''
+Ask how many slices of pizza the user started with and ask
+how many slices they have eaten.
+Work out how many slices they have left and display the answer
+in a user-friendly format.
+'''
+
