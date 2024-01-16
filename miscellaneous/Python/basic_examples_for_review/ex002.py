6602b9354fad8d56ed7220766e4b67cc9ac835da
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-15 21:37:20 -0300

    Update ex002.py

--- miscellaneous/Python/basic_examples_for_review/ex002.py
+++ miscellaneous/Python/basic_examples_for_review/ex002.py
@@ -1,3 +1,7 @@
 # Ask for the user’s first name and then ask for their surname and display the output message
 # Hello [First Name] [Surname].
 
+first_name = input("Provide your first name: ")
+surname = input("Now, please provide your surname: ")
+
+print("Hello,", first_name, surname)
