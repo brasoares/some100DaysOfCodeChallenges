33570c3b26dffdfa463054ffa5b71d8afae7708e
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-15 21:53:36 -0300

    Update ex005.py

--- miscellaneous/Python/basic_examples_for_review/ex005.py
+++ miscellaneous/Python/basic_examples_for_review/ex005.py
@@ -3,3 +3,12 @@ Ask the user to enter three numbers.
 Add together the first two numbers and then multiply this total by the third.
 Display the answer as 'The answer is [answer].
 '''
+
+n1 = int(input("Enter the first integer: "))
+n2 = int(input("Enter the second integer: "))
+n3 = int(input("Enter the third integer: "))
+
+sum = n1 + n2
+answer = sum * n3
+
+print(f"The answer is: {answer}.")
