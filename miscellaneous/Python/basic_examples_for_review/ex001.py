c1b33e2ffe096f447d30016f93f38a7f9e3aaaa3
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-15 21:35:40 -0300

    Update ex001.py

--- miscellaneous/Python/basic_examples_for_review/ex001.py
+++ miscellaneous/Python/basic_examples_for_review/ex001.py
@@ -1,5 +1,5 @@
 # Ask for the user’s first name and display the output message
-# Hello [First Name]
+# Hello [First Name].
 
 first_name = input("Provide your first name: ")
 print("Hello, ", first_name + ".")
