63b1292758366428dc1bc650d94b1d9fa86112c5
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-15 21:59:35 -0300

    Update ex006.py

--- miscellaneous/Python/basic_examples_for_review/ex006.py
+++ miscellaneous/Python/basic_examples_for_review/ex006.py
@@ -5,3 +5,10 @@ Work out how many slices they have left and display the answer
 in a user-friendly format.
 '''
 
+print("Please, just provide positive numbers!")
+starting_point = int(input("How many slices did you start with? "))
+eaten_amount = int(input("How many have you eaten? "))
+
+slices_left = startint_point - eaten_amount
+
+print(f"You have {slices_left} slices left :)!")
