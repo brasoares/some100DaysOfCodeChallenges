6935d9ecfbc5c34794567f866b8835786b1bdab6
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-15 21:50:37 -0300

    Update ex004.py

--- miscellaneous/Python/basic_examples_for_review/ex004.py
+++ miscellaneous/Python/basic_examples_for_review/ex004.py
@@ -3,8 +3,8 @@ Ask the user to enter two numbers.
 Add them together and display the answer as 'The total is [answer]'.
 '''
 
-n1 = input("Enter the first integer: ")
-n2 = input("Enter the second integer: ")
+n1 = int(input("Enter the first integer: "))
+n2 = int(input("Enter the second integer: "))
 
 sum = n1 + n2
 
