06dafb03229828820e2ec427eaa36d6bb46dc7b8
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-15 21:50:06 -0300

    Update ex004.py

--- miscellaneous/Python/basic_examples_for_review/ex004.py
+++ miscellaneous/Python/basic_examples_for_review/ex004.py
@@ -2,3 +2,10 @@
 Ask the user to enter two numbers.
 Add them together and display the answer as 'The total is [answer]'.
 '''
+
+n1 = input("Enter the first integer: ")
+n2 = input("Enter the second integer: ")
+
+sum = n1 + n2
+
+print(f"The total is: {sum}")
