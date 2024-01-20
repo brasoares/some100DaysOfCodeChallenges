d19e064ec31ca32718f8c7299f7cdef1b3755b69
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-19 23:15:10 -0300

    Update ex007.py

--- miscellaneous/Python/basic_examples_for_review/ex007.py
+++ miscellaneous/Python/basic_examples_for_review/ex007.py
@@ -1,1 +1,8 @@
+'''
+Ask the user for their name and their age. Add 1 to their age
+and display the output [Name] next birthday you will be [new age].
+'''
 
+current_age = input('Hi! Please, inform you current age: ')
+
+new_age = current_age + 1
