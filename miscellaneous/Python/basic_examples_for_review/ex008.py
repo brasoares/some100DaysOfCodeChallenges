11cd02bdfdc632f1c762ca397017f9e2d34e3af8
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-19 23:20:10 -0300

    Update ex008.py

--- miscellaneous/Python/basic_examples_for_review/ex008.py
+++ miscellaneous/Python/basic_examples_for_review/ex008.py
@@ -5,3 +5,7 @@ number of diners and show how much each
 person must pay.
 '''
 
+bill_amount = float(input("Enter bill's total price: "))
+diners_count = int(input("How many diners are there? "))
+
+pay_per_diner = bill_amount / diners_count
