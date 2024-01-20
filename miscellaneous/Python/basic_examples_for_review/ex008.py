96b1eed4c945b6e6597bd31a407b8ec7deb5f4f9
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-19 23:22:59 -0300

    Update ex008.py

--- miscellaneous/Python/basic_examples_for_review/ex008.py
+++ miscellaneous/Python/basic_examples_for_review/ex008.py
@@ -8,4 +8,10 @@ person must pay.
 bill_amount = float(input("Enter bill's total price: "))
 diners_count = int(input("How many diners are there? "))
 
-pay_per_diner = bill_amount / diners_count
+# If necessary, use while to wait for the correct positive number (pay_per_diner)
+
+if diners_count != 0:
+    pay_per_diner = bill_amount / diners_count
+    print(f"Each person must pay: ${pay_per_diner:.2f}")
+else:
+    print("Cannot divide by zero. Please, restart!")
