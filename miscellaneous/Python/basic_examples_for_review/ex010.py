b5d8e14e77104a6d981862163fc5066ffe3c08bf
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-20 20:56:21 -0300

    Update ex010.py

--- miscellaneous/Python/basic_examples_for_review/ex010.py
+++ miscellaneous/Python/basic_examples_for_review/ex010.py
@@ -3,3 +3,11 @@
  Ask the user to enter a weight in kilograms and convert it 
  to pounds.
 '''
+# Read input from the user
+weight_in_kilograms = float(input("Enter weight in kilograms: "))
+
+# Conversion
+weight_in_pounds = weight_in_kilograms * 2.204
+
+# Display the result
+print(f"{weight_in_kilograms} kilograms is equal to {weight_in_pounds} pounds.")
