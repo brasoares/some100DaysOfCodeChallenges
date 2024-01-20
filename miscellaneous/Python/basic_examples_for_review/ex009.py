99dfd9e6da0216c021cd70bcc47506c754dde4a2
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-19 23:30:57 -0300

    Update ex009.py

--- miscellaneous/Python/basic_examples_for_review/ex009.py
+++ miscellaneous/Python/basic_examples_for_review/ex009.py
@@ -3,6 +3,8 @@ Write a program that will ask for a number of days and then will
 show how many hours, minutes and seconds are in that number of days.
 '''
 
+num_of_days = int(input("Enter the number of days for calculation: "))
+
 # Calculate hours, minutes, and seconds
 hours = num_of_days * 24
 minutes = hours * 60
