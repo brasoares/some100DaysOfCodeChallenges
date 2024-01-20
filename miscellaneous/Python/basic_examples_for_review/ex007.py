4563ab9a6c9f48218e98b2c4eac387bad86ebda4
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-19 23:16:21 -0300

    Update ex007.py

--- miscellaneous/Python/basic_examples_for_review/ex007.py
+++ miscellaneous/Python/basic_examples_for_review/ex007.py
@@ -3,7 +3,7 @@ Ask the user for their name and their age. Add 1 to their age
 and display the output [Name] next birthday you will be [new age].
 '''
 
-current_age = input('Hi! Please, inform you current age: ')
+current_age = int(input('Hi! Please, inform you current age: '))
 
 new_age = current_age + 1
 print(f"On your next Birthday you'll be {new_age} years old :)")
