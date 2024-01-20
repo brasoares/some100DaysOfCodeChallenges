4013e3bfc7487e6d41e8889f07589d15ebac5b1f
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-19 23:26:34 -0300

    Update ex009.py

--- miscellaneous/Python/basic_examples_for_review/ex009.py
+++ miscellaneous/Python/basic_examples_for_review/ex009.py
@@ -2,3 +2,5 @@
 Write a program that will ask for a number of days and then will
 show how many hours, minutes and seconds are in that number of days.
 '''
+
+num_of_days = int(input("Enter the number of days for calculation: "))
