98728fba9477fabb797c80b37af77d9e2cd2e415
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-20 22:30:05 -0300

    Update ex011.py

--- miscellaneous/Python/basic_examples_for_review/ex011.py
+++ miscellaneous/Python/basic_examples_for_review/ex011.py
@@ -10,4 +10,4 @@ number_under = int(input("Enter a number under 10: "))
 if number_under = 
 times = number_over/number_under
 
-print(f"{number_under} goes {times} times inside {numberOver}!")
+print(f"{number_under} goes {times} times inside {number_over}!")
