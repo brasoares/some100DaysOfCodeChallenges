732089b3c9fa7ce6d37d23ea843a6e62228e3b61
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-20 22:30:45 -0300

    Update ex011.py

--- miscellaneous/Python/basic_examples_for_review/ex011.py
+++ miscellaneous/Python/basic_examples_for_review/ex011.py
@@ -7,7 +7,8 @@ number in a user-friendly format.
 number_over = int(input("Enter a number over 100: "))
 number_under = int(input("Enter a number under 10: "))
 
-if number_under = 
+if number_under == 0:
+  print("Cannot divide by zero. Try again!")
 times = number_over/number_under
 
 print(f"{number_under} goes {times} times inside {number_over}!")
