2a21a6f6736fb5935c9d1584665a347fc15aab1c
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-20 22:33:36 -0300

    Update ex011.py

--- miscellaneous/Python/basic_examples_for_review/ex011.py
+++ miscellaneous/Python/basic_examples_for_review/ex011.py
@@ -7,13 +7,11 @@ number in a user-friendly format.
 number_over = int(input("Enter a number over 100: "))
 
 While True:
+  number_under = int(input("Enter a number under 10: "))
   
-
-
-number_under = int(input("Enter a number under 10: "))
-
-if number_under == 0:
-  print("Cannot divide by zero. Try again!")
-else:
-  times = number_over/number_under
-  print(f"{number_under} goes around {times:.2f} times inside {number_over}!")
+  if number_under == 0:
+    print("Cannot divide by zero. Try again!")
+  else:
+    times = number_over/number_under
+    print(f"{number_under} goes around {times:.2f} times inside {number_over}!")
+    break
