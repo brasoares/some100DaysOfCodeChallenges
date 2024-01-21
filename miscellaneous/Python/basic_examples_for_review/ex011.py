e71ebeb123e34e233f20062003e257683ed25fb1
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-20 22:36:46 -0300

    Update ex011.py

--- miscellaneous/Python/basic_examples_for_review/ex011.py
+++ miscellaneous/Python/basic_examples_for_review/ex011.py
@@ -5,6 +5,7 @@ number in a user-friendly format.
 '''
 
 number_over = int(input("Enter a number over 100: "))
+number_under = 0
 
 while number_under <= 0:
   number_under = int(input("Enter a number under 10: "))
@@ -14,3 +15,4 @@ while number_under <= 0:
   else:
     times = number_over/number_under
     print(f"{number_under} goes around {times:.2f} times inside {number_over}!")
+
