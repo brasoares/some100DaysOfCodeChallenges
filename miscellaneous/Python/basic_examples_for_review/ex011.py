d501b77f569040f49ff43bbb810895e475528c45
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-20 22:27:20 -0300

    Update ex011.py

--- miscellaneous/Python/basic_examples_for_review/ex011.py
+++ miscellaneous/Python/basic_examples_for_review/ex011.py
@@ -1,1 +1,12 @@
+'''
+Task the user to enter a number over 100 and then enter a number under
+10 and tell them how many times the smaller number goes into larger
+number in a user-friendly format.
+'''
 
+numberOver = int(input("Enter a number over 100: "))
+numberLower = int(input("Enter a number under 10: "))
+
+times = numberOver/numberLower
+
+print(f"{numberLower} goes {times} times inside {numberOver}!")
